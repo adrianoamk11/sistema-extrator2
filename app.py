789fe048f19a4f91af86cc3ca7cf23ef637563e3
adrianoamk11 <adrianoamk11@gmail.com>
2026-08-08 16:31:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -448,7 +448,7 @@ def emitir_boleto_asaas(
             f"Faturamento {formatar_moeda(faturamento)}"
         )
 
-       external_reference = criar_referencia_externa(
+           external_reference = criar_referencia_externa(
         nome_arquivo,
         valor_cobranca,
         vencimento,
