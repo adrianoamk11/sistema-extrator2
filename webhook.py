878f973bce28ab7267df301db60168556a99fd1f
adrianoamk11 <adrianoamk11@gmail.com>
2026-08-10 12:07:21 -0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -58,7 +58,7 @@ def webhook_asaas():
         "value": pagamento.get("value"),
         "effectiveDate": date.today().isoformat(),
     }
-   resposta = requests.post(
+    resposta = requests.post(
     f"{ASAAS_BASE_URL}/invoices",
         
         headers=cabecalhos_asaas(),
