5aee49a1272088cf1f8cc93ff443e738bc1beca9
adrianoamk11 <adrianoamk11@gmail.com>
2026-08-10 11:35:27 -0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -53,7 +53,7 @@ def webhook_asaas():
 
     payload_nota = {
         "payment": payment_id,
-        "serviceDescription": pagamento.get("description") or "Royalties",
+       "description": pagamento.get("description") or "Royalties",
         "observations": "Royalties - Lider Franquia",
         "value": pagamento.get("value"),
         "effectiveDate": date.today().isoformat(),
