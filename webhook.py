6d6279a9b85706f22b6cc351196f668466b106dd
adrianoamk11 <adrianoamk11@gmail.com>
2026-08-10 09:01:58 -0300

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -66,12 +66,17 @@ def webhook_asaas():
         timeout=30,
     )
 
-    if not resposta.ok:
-        return jsonify({
-            "erro": "falha ao criar nota fiscal",
-            "status": resposta.status_code,
-            "detalhe": resposta.text,
-        }), 500
+   if not resposta.ok:
+    print("ERRO AO CRIAR NOTA FISCAL NO ASAAS")
+    print("STATUS:", resposta.status_code)
+    print("RESPOSTA:", resposta.text)
+
+    return jsonify({
+        "ok": True,
+        "nota": "falhou",
+        "status_asaas": resposta.status_code,
+        "detalhe": resposta.text,
+    }), 200
 
     return jsonify({
         "ok": True,
